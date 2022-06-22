9b98fcbb73b0d5aa03e2afe20981999f3b8e4a3d
handokoadjipangestu <handokoadjipangestu@gmail.com>
2022-06-22 13:37:53 +0700

    revision - change App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import $ from 'jquery'; 
+import React, { useState, useEffect } from "react";
+import $ from "jquery";
 
 import Home from "containers/pages/Home/index";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   useEffect(() => {
-    $(document).ready(function() {
+    $(document).ready(function () {
       function mobileViewUpdate() {
         const viewportWidth = $(window).width();
         if (viewportWidth < 600) {
-          $('.navbar.fixed-top').toggleClass('mobile');
+          $(".navbar.fixed-top").toggleClass("mobile");
         }
       }
-    
+
       $(window).on("load", mobileViewUpdate);
       $(window).on("resize", mobileViewUpdate);
+
+      mobileViewUpdate();
     });
   });
 
